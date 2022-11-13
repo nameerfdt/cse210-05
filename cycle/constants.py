d9ee933f69b342a9bd41b1024647a71be1237e28
Tracy Freeman <fre2108@byui.edu>
2022-11-12 20:00:57 -0800

    initial upload and updated Readme content

---
+++ cycle/constants.py
@@ -0,0 +1,19 @@
+from game.shared.color import Color
+
+
+COLUMNS = 40
+ROWS = 20
+CELL_SIZE = 15
+MAX_X = 900
+MAX_Y = 600
+FRAME_RATE = 5 #15
+FONT_SIZE = 15
+CAPTION = "Cycle"
+#SNAKE_LENGTH = 8
+CYCLE_LENGTH = 13
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+YELLOW = Color(255, 255, 0)
+GREEN = Color(0, 255, 0)
+PINK = Color(255, 20, 147)#added for cycle2
+CYAN = Color(0, 205,205) #added for cycle 2 
